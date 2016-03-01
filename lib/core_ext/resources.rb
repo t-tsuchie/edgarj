82bfbd71060b2825c94b36b8d76f50616f56ffc0
ido <f-ido@lab.acs-jp.com>
2016-03-01 11:00:41 +0900

    update comment

--- lib/core_ext/resources.rb
+++ lib/core_ext/resources.rb
@@ -1,20 +1,24 @@
 module ActionDispatch::Routing::Mapper::Resources
-  # add followings to work on Edgarj::EdgarjController derived class to
-  # default resource routing:
+  # define edgarj specific routing for CRUD.
   #
-  #   collection do
-  #     get :clear
-  #     get :csv_download
-  #     get :search
-  #     get :search_clear
-  #     get :search_save
-  #     get :search_load
-  #   end
+  # Following declaration in config/routes.rb:
   #
-  #   member do
-  #     put :page_info_save
-  #   end
+  #   edgarj_resources :photos
   #
+  # creates the following routes in addition to the default routes
+  # in your application,
+  # all mapping to the Photos controller which inherits from
+  # Edgarj::EdgarjController:
+  #
+  #   GET       /photos/clear
+  #   GET       /photos/csv_download
+  #   GET       /photos/search
+  #   GET       /photos/search_clear
+  #   GET       /photos/zip_complete
+  #   PATCH/PUT /photos/:id/page_info_save
+  #
+  # Where, :id is internally used for session so that client application
+  # doesn't have to take care it.
   def edgarj_resources(*symbols, &block)
     resources *symbols do
       yield if block_given?
@@ -30,29 +34,30 @@ module ActionDispatch::Routing::Mapper::Resources
       end
 
       member do
-        put :page_info_save
+        put   :page_info_save
+        patch :page_info_save
       end
     end
 
     self
   end
 
-  # add followings to work on Edgarj::PopupController derived class to
-  # default resource routing:
+  # define edgarj specific routing for popup.
   #
-  #   collection do
-  #     get :clear
-  #     get :csv_download
-  #     get :search
-  #     get :search_clear
-  #     get :search_save
-  #     get :search_load
-  #   end
+  # Following declaration in config/routes.rb:
   #
-  #   member do
-  #     put :page_info_save
-  #   end
+  #   edgarj_popup_resources :photos_popup
   #
+  # creates the following routes in your application,
+  # all mapping to the Photos controller which inherits from
+  # Edgarj::PopupController:
+  #
+  #   GET       /photos_popup/index
+  #   GET       /photos_popup/search
+  #   PATCH/PUT /photos_popup/:id/page_info_save
+  #
+  # Where, :id is internally used for session so that client application
+  # doesn't have to take care it.
   def edgarj_popup_resources(*symbols, &block)
     resources *symbols, only: [:index] do
       yield if block_given?
